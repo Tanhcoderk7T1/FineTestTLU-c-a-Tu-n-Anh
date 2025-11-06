c8c5e6ad651c4de4347f4b6950b174b105385b3c
Tanhcoderk7T1 <muizmusic2k7@gmail.com>
2025-11-06 22:39:36 +0700

    Add files via upload

---
+++ FineTestTLU/CONGTHUCPHUONGTRINHBAC2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main () {
+	int a, b, c, Y;
+	cout << " Nhap a = "; cin >> a;
+	cout << " Nhap b = "; cin >> b;
+	cout << " Nhap c = "; cin >> c;
+	if ( b <= c ) {
+	Y = a * a + 3 * a -1 ;
+	cout << " Y = " << Y ; }
+	else 
+	cout << " Y = 0 ";
+	return 0;
+}
+	
